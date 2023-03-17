7a019e6c38f9b41e385dcfab28412c2d22e1a228
olivierh94 <olivier.halfon@shopping-feed.com>
2023-03-17 16:28:43 +0100

    update cursor

---
+++ src/ForwardCursor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ShoppingFeed\Paginator;
+
+class ForwardCursor extends CursorSerializable
+{
+    public function __construct(int $limit)
+    {
+        $this->setValue('');
+        $this->setLimit($limit);
+        $this->setDirection(self::PAGE_NEXT);
+    }
+
+    public function withValue(string $value): self
+    {
+        $cursor = clone $this;
+        $cursor->setValue($value);
+
+        return $cursor;
+    }
+
+}
